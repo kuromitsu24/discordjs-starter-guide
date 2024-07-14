07048117dd8c5af632babe1eb6a7ed48704b43a7
Kurokami Mitsuki <170867643+kuromitsu24@users.noreply.github.com>
2024-07-14 14:14:16 +0700

    Update ping.js

--- slash_commands/miscellaneous/ping.js
+++ slash_commands/miscellaneous/ping.js
@@ -1,1 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
+module.exports = {
+  run: async (client, interaction) => {
+    const latency = await client.ws.ping;
+
+    await interaction.reply({
+      content: `Pong ${latency}ms!`,
+      ephemeral: false
+    });
+  },
+  data: new SlashCommandBuilder()
+  .setName('ping')
+  .setDescription('Used to check API latency between Client and Discord interaction times in Milliseconds.')
+};
